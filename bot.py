d8162f016c10b0ea284f681c3af22d3211cadcb3
sanflan <sandalinasfrancisco@gmail.com>
2021-01-11 12:28:43 -0300

    vc check when entry comes up.

--- bot.py
+++ bot.py
@@ -5,14 +5,20 @@ import random
 import os
 from dotenv import load_dotenv
 
-class Serie:
-    def __init__(self, user, serie, ticket):
+
+class Entry:
+    def __init__(self, user, entry, ticket):
         self.user = user
-        self.serie = serie
+        self.entry = entry
         self.ticket = 0
 
     def __str__(self):
-        return 'userid: {} serie: {} tickets: {}'.format(self.user,self.serie,self.ticket)
+        return 'userid: {} entry: {} tickets: {}'.format(
+                self.user,
+                self.entry,
+                self.ticket
+                )
+
 
 db = []
 
@@ -22,31 +28,49 @@ token = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='?!', description='series', intents=intents)
+bot = commands.Bot(command_prefix='?!', description='entrys', intents=intents)
 
 @bot.command(name='roll')
 async def roll(ctx):
     entries = len(db)
     if entries == 0:
-        await ctx.send("No hay series")
+        await ctx.send("No hay entrys")
         return
     roll = random.randint(0,entries-1)
-    serie = db[roll]
-    await ctx.send("Salio la serie {} propuesta por <@{}>".format(serie.serie, serie.user))
-    db.remove(serie)
+    entry = db[roll]
+    await ctx.send("{} propuesta por <@{}>".format(entry.entry, entry.user))
+    member = ctx.guild.get_member(entry.user)
+    if(member.voice != None):
+        await ctx.send("Preparate el pochoclo <@{}> que salio tu serie".format(
+            entry.user
+            ))
+        db.remove(entry)
+        return
+    await ctx.send("Parece que <@{}> no esta en vc...".format(
+        entry.user
+        ))
 
 @bot.command()
-async def add(ctx, serie: str):
-    tmp = Serie(ctx.author.id, serie, 0)
+async def add(ctx, entry: str):
+    tmp = Entry(ctx.author.id, entry, 0)
     db.append(tmp)
-    await ctx.send("Se Agrego La Serie!")
+    await ctx.send("Se Agrego La Entry!")
 
 @bot.command(aliases=['ldb'])
-async def listdb(ctx):
-    for serie in db:
-        print(serie)
-        print("propuesta por {}".format(bot.get_user(serie.user)))
+async def list_db(ctx):
+    for entry in db:
+        print(entry)
+        print("propuesta por {}".format(bot.get_user(entry.user)))
     await ctx.send("Mira los logs")
 
+@bot.command(aliases=['vchk'])
+async def is_in_voice(ctx, user: discord.User):
+    print(ctx.guild.get_member(user.id))
+    member = ctx.guild.get_member(user.id)
+    if(member.voice != None):
+        await ctx.send("El usuario mencionado esta en voice!")
+        return
+    await ctx.send("El usuario mencionado no esta en voice :(")
+    return
 
 bot.run(token)
